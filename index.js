304473eee9578f12e81e02e5523d45e5770bf863
Randolf Joshua Diezmo <joshua.diezmo@zynappse.com>
2017-08-30 17:29:03 +0800

    add quality auto

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function optionsFromArguments(args) {
     options = requiredOrFromEnvironment(options, 'api_key', 'CLOUDINARY_API_KEY');
     options = requiredOrFromEnvironment(options, 'api_secret', 'CLOUDINARY_API_SECRET');
     options = fromEnvironmentOrDefault(options, 'development', 'IS_DEVELOPMENT', true);
+    options = fromEnvironmentOrDefault(options, 'quality_auto', 'QUALITY_AUTO', true);
     return options;
 }
 
@@ -34,13 +35,13 @@ function Cloudinary(data) {
     var options = optionsFromArguments(data || {});
     this.cloudinary_config = options;
     this.TempFolder = 'temp';
-    mkdirp(__dirname + path.sep + this.TempFolder, function(err) {
+    mkdirp(__dirname + path.sep + this.TempFolder, function (err) {
         console.log(err);
     });
     cloudinary.config({cloud_name: options.cloud_name, api_key: options.api_key, api_secret: options.api_secret});
 }
 
-Cloudinary.prototype.createFile = function(filename, data) {
+Cloudinary.prototype.createFile = function (filename, data) {
     var options = {};
 
     var type = this.getResourceType(filename);
@@ -63,36 +64,36 @@ Cloudinary.prototype.createFile = function(filename, data) {
     }
     var self = this;
     return new Promise((resolve, reject) => {
-            var fileTempPath = __dirname + path.sep + self.TempFolder + path.sep + filename.replace(/\//g, '~');
-    fs.writeFile(fileTempPath, data, function(err) {
-        if (err) {
-            console.log("UPLOAD_ERROR", err);
-            return reject(err);
-        } else {
-            try {
-                console.log(options)
-                cloudinary.uploader.upload(fileTempPath, function(result) {
+        var fileTempPath = __dirname + path.sep + self.TempFolder + path.sep + filename.replace(/\//g, '~');
+        fs.writeFile(fileTempPath, data, function (err) {
+            if (err) {
+                console.log("UPLOAD_ERROR", err);
+                return reject(err);
+            } else {
+                try {
+                    console.log(options)
+                    cloudinary.uploader.upload(fileTempPath, function (result) {
+                        fs.unlink(fileTempPath);
+                        if (result.error) {
+                            reject(result.error);
+                        } else
+                            resolve(data)
+                    }, options);
+                } catch (e) {
                     fs.unlink(fileTempPath);
-                    if (result.error) {
-                        reject(result.error);
-                    } else
-                        resolve(data)
-                }, options);
-            } catch (e) {
-                fs.unlink(fileTempPath);
-                reject(e);
+                    reject(e);
+                }
             }
-        }
+        });
     });
-});
 }
 
-Cloudinary.prototype.deleteFile = function(filename) {
+Cloudinary.prototype.deleteFile = function (filename) {
     var folder = this.folder;
-    return new Promise(function(resolve, reject) {
-        ifWebp(folder, filename).then(function(_filename) {
+    return new Promise(function (resolve, reject) {
+        ifWebp(folder, filename).then(function (_filename) {
             filename = _filename;
-            fs.unlink(__dirname + path.sep + ".." + path.sep + folder + path.sep + filename, function(err) {
+            fs.unlink(__dirname + path.sep + ".." + path.sep + folder + path.sep + filename, function (err) {
                     if (err) {
                         console.log('err', err);
                         reject(err)
@@ -104,13 +105,13 @@ Cloudinary.prototype.deleteFile = function(filename) {
     });
 }
 
-Cloudinary.prototype.getFileData = function(filename) {
+Cloudinary.prototype.getFileData = function (filename) {
     var folder = this.folder;
-    return new Promise(function(resolve, reject) {
-        ifWebp(folder, filename).then(function(_filename) {
+    return new Promise(function (resolve, reject) {
+        ifWebp(folder, filename).then(function (_filename) {
             filename = _filename;
             try {
-                fs.readFile(__dirname + path.sep + ".." + path.sep + folder + path.sep + filename, null, function(err, data) {
+                fs.readFile(__dirname + path.sep + ".." + path.sep + folder + path.sep + filename, null, function (err, data) {
                         if (err) {
                             console.log('err', err);
                             reject(err);
@@ -126,8 +127,9 @@ Cloudinary.prototype.getFileData = function(filename) {
     });
 }
 
-Cloudinary.prototype.getFileLocation = function(config, filename) {
-    return ("https://res.cloudinary.com/" + this.cloudinary_config.cloud_name + "/" + this.getResourceType(filename) + "/upload/" + encodeURIComponent(filename));
+Cloudinary.prototype.getFileLocation = function (config, filename) {
+    let {quality_auto} = this.cloudinary_config;
+    return ("https://res.cloudinary.com/" + this.cloudinary_config.cloud_name + "/" + this.getResourceType(filename) + "/upload/" + (quality_auto ? "q_auto/" : "") + encodeURIComponent(filename));
 }
 
 Cloudinary.prototype.getResourceType = (filename) => {
